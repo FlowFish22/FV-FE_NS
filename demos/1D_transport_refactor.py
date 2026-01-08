c2979fa5551a0786c5f89bdab34a414da4d8f670
Mainak Kar <tapukor.moinak11@gmail.com>
2026-01-08 12:32:22 +0100

    working on the discretization of the initial data

--- demos/1D_transport_refactor.py
+++ demos/1D_transport_refactor.py
@@ -13,17 +13,22 @@ import finite_volume.finite_volume as fv
 # Option 2: input is in the demo script
 tf = 2.0
 initial_condition = fv.initial_condition.disp_Riemann
-case = fv.computational_case(N = 1000)
-#Discretize initial condition
+case = fv.computational_case(N = 3)
 N = case.N
-x = np.linspace(0, 1, num=int(1e2))
-(rho0,u0) = initial_condition(x)
-f, ax = plt.subplots(layout="constrained")
-ax.plot(x, rho0, label=r"$\rho$")
-ax.plot(x, u0, label="u")
-ax.set_xlabel("x")
-ax.set_title("Initial condition")
-ax.legend()
+#Discretize the initial density by taking cell averages on primal cells
+x_prim = np.array([((i + 0.5) * (1.0 /N)) for i in range(0, N)])
+b = spi.quad(np.sin, 0, 1)
+r_avg = np.array([spi.quad(lambda x: x, i/N, (i+1.0)/N)[0] for i in range(0,N)])
+
+#----------------------------------
+#x = np.linspace(0, 1, num=int(1e2))
+#rho0 = initial_condition(x)[1]
+#f, ax = plt.subplots(layout="constrained")
+#ax.plot(x, rho0, label=r"$\rho$")
+#ax.plot(x, u0, label="u")
+#ax.set_xlabel("x")
+#ax.set_title("Initial condition")
+#ax.legend()
 
 # Plot the result
 # u = results.u
